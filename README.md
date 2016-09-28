00ff044ce1d5fcc7c6e5dee762e92658c102541a
Devon McGrath <devon.mcgrath@gmail.com>
2016-09-29 10:59:26 +1300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+#WotWot version 0.0
+
+A project currently in development
+
+## project setup
+
+* Clone Repository
+* Create your branch
+* npm install
+* execute command "npm run build"
+* execute command "npm run serve"
+* navigate to localhost:8080
+
+
+
+
+
+
+
+
+
+##Thank you Don for the project starter
---
+++ client/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import thunk from 'redux-thunk'
+
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+
+// import actions from './actions/...........'
+import actions from './actions'
+
+import appReducer from './reducers/combineReducers'
+
+import App from './components/App'
+import PersonalitiesIndex from './containers/PersonalitiesIndexContainer'
+import ThingDetail from './containers/ThingDetailContainer'
+
+let store = createStore(
+  appReducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ))
+
+const history = syncHistoryWithStore(hashHistory, store)
+
+document.addEventListener('DOMContentLoaded', () => {
+  render(
+    <Provider store={store}>
+      <Router history={history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={PersonalitiesIndex} />
+          <Route name="detail" component={ThingDetail} path="details/:id" />
+        </Route>
+      </Router>
+    </Provider>,
+    document.getElementById('app')
+  )
+})
