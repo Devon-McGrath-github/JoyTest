1c44d8bfc9485223281570945e3e0b148b6c03cd
Devon McGrath <devon.mcgrath@gmail.com>
2016-09-29 14:32:44 +1300

    cleaned up the code. Removed the crap

--- client/reducers/PlaceholderReducer.js
+++ client/reducers/PlaceholderReducer.js
@@ -5,49 +5,13 @@ const driveImages = [
 ]
 
 const INITIAL_STATE = {
-  selectedThing: null,
-  things: [{
-    id: 1,
-    name: 'Red thing'
-  }],
-  auth: {},
   images: driveImages,
   nextPage: driveImages[0]
 }
 
 export default (state = INITIAL_STATE , action) => {
   switch (action.type) {
-    case SHOW_DETAILS:
-      return Object.assign({}, state, {
-        selectedThing: action.id
-      })
-    case HIDE_DETAILS:
-      return Object.assign({}, state, {
-        selectedThing: null
-      })
-    case ADD_THING:
-    // This is an alternative that uses the ES6 spread operator
-    // return Object.assign({}, state, {
-    //   things: [
-    //     ...state.things,
-    //     {
-    //       id: state.things.length + 1,
-    //       name: action.name
-    //     }
-    //   ]
-    // })
 
-      // create copy of existing state
-      const newState = Object.assign({}, state)
-      newState.things = state.things.slice()
-
-      // add new thing
-      newState.things.push({
-        id: state.things.length + 1,
-        name: action.name
-      })
-
-      return newState
     default:
       return state
   }
